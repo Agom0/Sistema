e01bde192d3e004d00db84481d495ec759419e2d
Agom0 <alancruzz407@gmail.com>
2025-05-02 11:50:42 -0300

    Fim da aula de vit

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,3 +1,5 @@
+
+
 export default function CadastroPage () {
     return (
     <p>Aqui é uma pagina de cadastro</p>
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,25 @@
-export default function LoginPage (){
+import { Link, useNavigate } from "react-router-dom"
+import svg from "/public/fingerprint-animate.svg"
+
+export default function LoginPage() {
+    const navigate = useNavigate() // forma bem leve e boa de criar o botão
     return (
-        <p>Aqui é uma pagina de login</p>
-    )
+
+        <div className="w-full h-screen flex">
+            <div className="w-[40%] h-full flex items-center justify-center">
+                <Link to="/cadastro">Cadastrar</Link>
+                <button
+                    onClick={() => navigate("/cadastro")}
+                > Cadastrar</button>
+                <button
+                    onClick={() => window.location.href = "/cadastro"} //Forma mais leve, comum e usada para adicionar botão de biblioteca usando o location
+                >
+                    Cadastrar
+                </button>
+            </div>
+            <div className="w-[60%] h-full bg-[#dedada] flex items-center justify-center rounded-l-lg flex-col">
+                <img width={600} src={svg} alt="imagem-animada" />
+                <p className="text-white font-bold w-[300px] text-[20px] text-center">A melhor experiencia de login que você já teve na sua vida</p>
+            </div>
+        </div>)
 }
